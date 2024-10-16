42d5d1dffc955f8d7ee8c9da11dfc61089984b07
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-16 23:11:24 +0400

    aded the project for the first time

---
+++ WeaponProject/Models/Weapon.cs
@@ -0,0 +1,113 @@
+﻿namespace WeaponProject.Models;
+
+class Weapon
+{
+    private int _maxAmmo;
+    private int _ammo;
+    private int _magFixedMtSec;
+    private bool _isAuto;
+
+    public Weapon(int maxAmmo, int ammo, int magFixedMtSec, bool isAuto)
+    {
+        MaxAmmo = maxAmmo;
+        Ammo = ammo;
+        MagFixedMtSec = magFixedMtSec;
+        _isAuto = isAuto;
+    }
+
+    public int MaxAmmo
+    {
+        get { return _maxAmmo; }
+        set { _maxAmmo = value; }
+    }
+
+    public int Ammo
+    {
+        get { return _ammo; }
+        set
+        {
+            if (value > _maxAmmo)
+            {
+                Console.WriteLine("What a waste of ammo !!!");
+                _ammo = _maxAmmo;
+            }
+            else if (value < 0)
+            {
+                Console.WriteLine("Cant be less than 0!");
+                _ammo = 0;
+            }
+            else
+                _ammo = value;
+        }
+    }
+
+    public int MagFixedMtSec
+    {
+        get { return _magFixedMtSec; }
+        set { _magFixedMtSec = value; }
+    }
+
+    public bool IsAuto
+    {
+        get { return _isAuto; }
+        set { _isAuto = value; }
+    }
+
+    public void Shoot()
+    {
+        if (Ammo == 0)
+            AutoReload();
+        else
+        {
+            Ammo--;
+            Console.WriteLine("Fired one bullet!");
+        }
+    }
+    public void Fire()
+    {
+        if (Ammo == 0)
+            AutoReload();
+        if (!IsAuto) Shoot();
+        else
+        {
+            float magMtSec;
+            magMtSec = Convert.ToSingle(Ammo * MagFixedMtSec) / MaxAmmo;
+            Ammo = 0;
+            Console.WriteLine($"Fired all of the remining bullets in {magMtSec}");
+        }
+    }
+    public int GetRemainBulletCount()
+    {
+        return MaxAmmo - Ammo;
+    }
+
+    public void MagInfo()
+    {
+        Console.WriteLine("Current bullets: " + Ammo);
+    }
+
+    public void Reload()
+    {
+        if (Ammo < MaxAmmo) Ammo = MaxAmmo;
+        {
+            Console.WriteLine("Reload!");
+        }
+    }
+    public void AutoReload()
+    {
+        Console.WriteLine("No ammo to shoot!");
+        Console.WriteLine("Auto reloading!");
+        Reload();
+    }
+    public void AddAmmo(int ammo)
+    {
+        Ammo += ammo;
+    }
+    public void ChangeFireMode(bool isAuto)
+    {
+        IsAuto = isAuto;
+        if (isAuto) Console.WriteLine("Fire mode: Auto");
+        else Console.WriteLine("Fire mode: Single");
+    }
+}
+
---
+++ WeaponProject/Program.cs
@@ -0,0 +1,80 @@
+﻿using WeaponProject.Models;
+
+namespace Test2;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("==============================START==============================");
+        Console.Write("Max Ammo: ");
+        int maxAmmo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ammo: ");
+        int ammo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Mag Fixed Empty Seconds: ");
+        int magFixedMtSec = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Is Auto(true/false): ");
+        bool isAuto = Convert.ToBoolean(Console.ReadLine());
+        Weapon weapon = new Weapon(maxAmmo, ammo, magFixedMtSec, isAuto);
+        ShowControls();
+
+        while (true)
+        {
+
+            bool stopTheGame = false;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 0:
+                    weapon.Shoot();
+                    break;
+                case 1:
+                    weapon.Fire();
+                    break;
+                case 2:
+                    Console.Write("Is Auto(true/false): ");
+                    bool Auto = Convert.ToBoolean(Console.ReadLine());
+                    Console.WriteLine(Auto);
+                    weapon.ChangeFireMode(Auto);
+                    Console.WriteLine(weapon.IsAuto);
+                    break;
+                case 3:
+                    if (weapon.GetRemainBulletCount() == 0) Console.WriteLine("Mag is full!");
+                    Console.WriteLine("You need " + weapon.GetRemainBulletCount() + " bullets for a full mag!"); ;
+                    break;
+                case 4:
+                    weapon.MagInfo();
+                    break;
+                case 5:
+                    weapon.Reload();
+                    break;
+                case 6:
+                    stopTheGame = true;
+                    Console.WriteLine("==============================END==============================");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input!");
+                    Console.WriteLine("Here is a list of the control inputs! (and a cat)");
+                    ShowControls();
+                    break;
+            }
+            if (stopTheGame) break;
+        }
+    }
+
+    static void ShowControls()
+    {
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("""
+                          0 - Shoot
+                          1 - Fire
+                          2 - Fire Mode              /\_/\
+                          3 - Remaining bullets     ( o.o )
+                          4 - Info                    >^<
+                          5 - Reload
+                          6 - Exit
+                          """);
+        Console.WriteLine("-------------------------------------");
+    }
+}
